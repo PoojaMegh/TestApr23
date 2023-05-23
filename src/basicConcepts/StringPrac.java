15372005bfa1316f33e2ae705309ffff3624f213
Pooja Sharma <pooja.meghrajsharma@gmail.com>
2023-05-23 08:48:41 +0530

    scenario2 conflict

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -3,7 +3,8 @@ package basicConcepts;
 public class StringPrac {
 
 	public static void main(String[] args) {
-		System.out.println("I am in branch A");
-		System.out.println("Conflict resolved");
+		System.out.println("I am in branch A2.....");
+		System.out.println("Conflict again");
+		System.out.println("Branch A2");
 	}
 }
