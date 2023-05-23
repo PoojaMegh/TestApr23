88d67ba02a8297b5454f7835e25ea72d25de27e6
Pooja Sharma <pooja.meghrajsharma@gmail.com>
2023-05-23 08:38:44 +0530

    Update the string prac file

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -3,9 +3,7 @@ package basicConcepts;
 public class StringPrac {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("I am in branch A");
-		System.out.println("Conflict changes");
+		System.out.println("Conflict resolved");
 	}
-
 }
