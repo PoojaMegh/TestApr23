c2d105b90449ad32a10b60142ab328d775140576
Pooja Sharma <pooja.meghrajsharma@gmail.com>
2023-05-23 08:53:24 +0530

    Branch B2 conflict

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -3,7 +3,8 @@ package basicConcepts;
 public class StringPrac {
 
 	public static void main(String[] args) {
-		System.out.println("I am in branch A");
+		System.out.println("I am in branch B");
 		System.out.println("Conflict resolved");
+		System.out.println("Conflict on branch B2");
 	}
 }
