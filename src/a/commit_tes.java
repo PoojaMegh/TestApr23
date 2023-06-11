755ec5d7eddb00332025797d66e4f44e6dadbe48
Pooja Sharma <pooja.meghrajsharma@gmail.com>
2023-06-11 08:53:11 +0530

    icommit_1

---
+++ src/a/commit_tes.java
@@ -0,0 +1,16 @@
+package a;
+
+public class commit_tes {
+	
+	final public int x=10;
+	static public int y=20;
+	
+	public void setVariable(int x, int y) {
+	//	this.y = 80;
+	}
+	
+	public static void main(String[] args) {
+		commit_tes a = new commit_tes();
+		System.out.print(a.x);
+	}
+}
